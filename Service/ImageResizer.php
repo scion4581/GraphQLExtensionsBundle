00cd14ec6adb092ee4fbc148323c0ecac4d28507
Alexandr Viniychuk <a@viniychuk.com>
2017-03-31 11:56:32 -0400

    updated image resizer

--- Service/ImageResizer.php
+++ Service/ImageResizer.php
@@ -9,7 +9,12 @@
 namespace Youshido\GraphQLExtensionsBundle\Service;
 
 
+use Imagine\Filter\Basic\Crop;
+use Imagine\Filter\Basic\Resize;
 use Imagine\Image\Box;
+use Imagine\Image\BoxInterface;
+use Imagine\Image\ImageInterface;
+use Imagine\Image\Point;
 use Youshido\GraphQLExtensionsBundle\Model\PathAwareInterface;
 use Youshido\GraphQLExtensionsBundle\Service\Locator\Storage\StorageInterface;
 use Youshido\GraphQLExtensionsBundle\Service\PathResolver\PathResolverInterface;
@@ -42,12 +47,42 @@ class ImageResizer
 
         $imagine = ImagineFactory::createImagine($this->driver);
 
+        /** @var ImageInterface $image */
+        $image = $imagine->open($this->pathResolver->resolveAbsolutePath($object));
         $this->storage->preparePath($absoluteTargetPath);
 
-        $imagine
-            ->open($this->pathResolver->resolveAbsolutePath($object))
-            ->thumbnail(new Box($config['width'], $config['height']), $config['mode'])
-            ->save($absoluteTargetPath);
+        $box = new Box($config['width'], $config['height']);
+        // determine scale
+        $size = $this->fillBox($image->getSize(), $box);
+        // define filters
+        $resize = new Resize($size);
+        if (empty($config['origin'])) {
+            $config['origin'] = [0.5, 0.5];
+        }
+        $origin = new Point(
+            floor(($size->getWidth() - $box->getWidth()) * $config['origin'][0]),
+            floor(($size->getHeight() - $box->getHeight()) * $config['origin'][0])
+        );
+        $crop = new Crop($origin, $box);
+        $image = $resize->apply($image);
+        $image = $crop->apply($image);
+
+        $image->save($absoluteTargetPath);
+    }
+
+    /**
+     * @param BoxInterface $sourceBox
+     * @param BoxInterface $targetBox
+     * @return BoxInterface
+     */
+    private function fillBox(BoxInterface $sourceBox, BoxInterface $targetBox) {
+        $sourceAspect = $sourceBox->getWidth() / $sourceBox->getHeight();
+        $targetAspect = $targetBox->getWidth() / $targetBox->getHeight();
+
+        if ($sourceAspect > $targetAspect) {
+            return $sourceBox->heighten($targetBox->getHeight());
+        }
+        return $sourceBox->widen($targetBox->getWidth());
     }
 
     public function getPathResolver()
